62b8208b071f103ed54aff3a51bd60ff6d8c59e6
fayob <adedejifavour6@gmail.com>
2025-04-04 23:32:48 +0100

    Add contract and app dependencies

---
+++ airdrop/contracts/Token.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Token is ERC20("Fabim", "FBM") {
+    constructor(uint256 initialSupply) {
+        _mint(msg.sender, initialSupply);
+    }
+}
